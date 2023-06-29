305addff99b4cadae01fb96c72d21672f4e7e072
Furcifu12 <capt.fantastic48@gmail>
2023-06-29 19:14:50 +0700

    Commit pertama

---
+++ html-final-project-library-app-v1/deployData.js
@@ -0,0 +1,15 @@
+/* 
+  buatlah beberapa variable untuk pengecekan deployment
+
+  studentName: berisi string nama student
+  studentId: berisi string CAMPID
+  NetlifyDeployUrl: berisi string url Netlify deployment
+*/
+
+const studentName = 'Muhamad Aldo Fernanda'; // TODO: replace this
+
+const studentId = 'FE5610889'; // TODO: replace this
+
+const NetlifyDeployUrl = 'https://aldo-fe5610889.netlify.app/'; // TODO: replace this
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
